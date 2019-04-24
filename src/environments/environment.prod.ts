a2451540622c56a443e8dd6104948ed59aca8b7a
bogdan-molodets <bogdan.molodets@gmail.com>
2019-04-24 20:14:23 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://monairapp.herokuapp.com/',
+  apiUrl: 'https://monairapp.herokuapp.com/api/',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYm9nZGFubW9sb2RldHMiLCJhIjoiY2pjMG9kZ3NjMDNhazJ4cXltNWdhYXh0diJ9.RbZ5rCF0N3-n5GKfGyrI3w'
   }
